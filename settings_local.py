cb12a1d292feb9aec1f9c6927e634d4fe9d69b83
Nick Pope <npope@ord-uber-vm001.fedcloud.cgipdc.cginet>
2016-12-20 18:43:10 -0700

    settings_local added to gitignore; settings_local for server 1 updated.

--- settings_local.py
+++ settings_local.py
@@ -2,10 +2,9 @@ import os
 
 # Define ENVIRONMENTAL VARIABLES for project (replaces the app.yaml)
 os.environ.update({
-    # 'CTS_TEST_SERVER': 'http://172.20.100.16:8080',
-    'CTS_TEST_SERVER': '',  # currently not available locally
-    'CTS_EPI_SERVER': 'http://localhost:55342',
-    'CTS_EFS_SERVER': 'http://ca-test-1.cloudapp.net',
-    'CTS_JCHEM_SERVER': 'http://ca-test-1.cloudapp.net',
+    'CTS_TEST_SERVER': 'http://172.20.100.16:8080',
+    'CTS_EPI_SERVER': 'http://172.20.100.18',
+    'CTS_EFS_SERVER': 'http://172.20.100.12',
+    'CTS_JCHEM_SERVER': 'http://172.20.100.12',
     'CTS_SPARC_SERVER': 'http://204.46.160.69:8080',
 })
