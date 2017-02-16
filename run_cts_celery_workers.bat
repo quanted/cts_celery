fde3940d56a8f5187b4ad2d95cd03c11b50439de
popenc <i.nickpope@gmail.com>
2017-02-16 14:58:21 -0500

    updated cts calcs ref

--- run_cts_celery_workers.bat
+++ run_cts_celery_workers.bat
@@ -11,6 +11,7 @@ rem start call ../config/set_env_vars.bat env_filename&&celery -A tasks worker -
 :: Runs celery worker with env vars from config/
 start call run_worker.bat manager %env_filename%
 start call run_worker.bat chemaxon %env_filename%
+start call run_worker.bat measured %env_filename%
 :: start call ../config/set_env_vars.bat env_filename&&celery -A tasks worker -Q chemaxon --loglevel=info --concurrency=1 -n chemaxon_worker
 :: start celery -A tasks worker -Q sparc --loglevel=info --concurrency=1 -n sparc_worker
 :: start celery -A tasks worker -Q epi --loglevel=info --concurrency=1 -n epi_worker
