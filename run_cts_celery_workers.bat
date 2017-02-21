1a112cec81cd20b3d97fa365f450cfed72f51a6c
popenc <i.nickpope@gmail.com>
2017-02-21 12:06:18 -0500

    ph ws from chemaxon units properly converted; temp config updated

--- run_cts_celery_workers.bat
+++ run_cts_celery_workers.bat
@@ -2,18 +2,14 @@
 :: Jun. 2016; np
 
 :: Filename for .env to use when running celery workers:
-set env_filename=%1
+rem set env_filename=%1
 
-echo .env Filename
-echo env_filename
+rem echo .env Filename
+rem echo env_filename
 
-rem start call ../config/set_env_vars.bat env_filename&&celery -A tasks worker -Q manager --loglevel=info -n manager_worker
-:: Runs celery worker with env vars from config/
-start call run_worker.bat manager %env_filename%
-start call run_worker.bat chemaxon %env_filename%
-start call run_worker.bat measured %env_filename%
-:: start call ../config/set_env_vars.bat env_filename&&celery -A tasks worker -Q chemaxon --loglevel=info --concurrency=1 -n chemaxon_worker
-:: start celery -A tasks worker -Q sparc --loglevel=info --concurrency=1 -n sparc_worker
+start celery -A tasks worker -Q manager --loglevel=info --concurrency=1 -n manager_worker
+start celery -A tasks worker -Q chemaxon --loglevel=info --concurrency=1 -n chemaxon_worker
+start celery -A tasks worker -Q sparc --loglevel=info --concurrency=1 -n sparc_worker
 :: start celery -A tasks worker -Q epi --loglevel=info --concurrency=1 -n epi_worker
 :: start celery -A tasks worker -Q measured --loglevel=info --concurrency=1 -n measured_worker
 :: start celery -A tasks worker -Q test --loglevel=info --concurrency=1 -n test_worker
