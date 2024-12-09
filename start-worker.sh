efe4e02d720b344447ba43f3595a0174a8eee0de
popenc <i.nickpope@gmail.com>
2024-12-09 14:45:52 -0500

    increased concurrency for worker

--- start-worker.sh
+++ start-worker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-micromamba run -n pyenv celery -A tasks worker -Q cts_queue -l info -n cts_worker -c 1
+micromamba run -n pyenv celery -A tasks worker -Q cts_queue -l info -n cts_worker -c 4
